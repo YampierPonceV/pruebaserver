a710507f66fef018f58c4122de3ddea3336a8b8a
yampierponcev <developer.yampier@gmail.com>
2024-03-18 09:10:51 -0500

    desde windows

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return 'Hola, bienvenido'
+	return 'Hola, bienvenido, mensaje desde visual estudio code'
 
 if __name__ == '__main__':
   	app.run(host='0.0.0.0',port=5000, debug=True)
